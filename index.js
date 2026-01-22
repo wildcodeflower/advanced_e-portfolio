9b8e833e6ac3b8a11ed0e532d14e444d38463949
wildcodeflower <codyglane@gmail.com>
2026-01-22 12:46:04 -0800

    1st revision project complete

--- index.js
+++ index.js
@@ -2,35 +2,61 @@
 // service_ynsqhzr
 // Q--5LHDPksFwdL8p3
 
-function contact(event) {
-event.preventDefault();
-const loading = document.querySelector('.modal__overlay--loading')
-const success = document.querySelector('.modal__overlay--success')
-loading.classList += " modal__overlay--visible"
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
 
-emailjs
-.sendForm(
-    'service_ynsqhzr',
-    'template_ld0p19g',
-    event.target,
-    'Q--5LHDPksFwdL8p3'
-).then(() => {
-    loading.classList.remove("modal__overlay--visible")
-    success.classList += " modal__overlay--visible"
-}).catch(() => {
-    loading.classList.remove("modal__overlay--visible")
-    alert(
-        "The contact form service is temporarily unavailble. Please contact me directly at codyglane@gmail.com"
-    )
-})
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
 }
 
-let isModalOpen = false
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_ynsqhzr",
+      "template_ld0p19g",
+      event.target,
+      "Q--5LHDPksFwdL8p3",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The contact form service is temporarily unavailble. Please contact me directly at codyglane@gmail.com",
+      );
+    });
+}
+
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
